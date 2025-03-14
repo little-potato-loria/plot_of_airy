f87c07bdf0d89fde29941724e20c1c2cb4160e1a
little-potato-loria <tudoutudouyaaaaa@163.com>
2025-03-14 18:08:16 +0800

    Update globle.c

--- globle.c
+++ globle.c
@@ -1,7 +1,7 @@
-#include "globle.h"
+#include "global.h"
 
-int NUM = 1024;
+
 double dt = 0.01;
 double yrem[NUM] = { 0 };
-float x0 = 1,v0=1, y0 = 1;
-float min_x=-2, max_x=2, min_y=-1, max_y=1;
+float x0 = 1,v0=1, y00 = 1;
+float min_x=-10, max_x=10, min_y=-5, max_y=5;
