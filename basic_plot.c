8e3016636cebd47060177e5580348d7be8298eeb
little-potato-loria <tudoutudouyaaaaa@163.com>
2025-03-14 18:07:05 +0800

    Update basic_plot.c

--- basic_plot.c
+++ basic_plot.c
@@ -1,37 +1,37 @@
-#include "globle.h"
+#include "global.h"
 #include "plot.h"
 
 void drawAxes() {
-    glColor3f(1.0, 1.0, 1.0);
+    glColor3f(1.0, 0, 0);
     glBegin(GL_LINES);
-    glVertex2f(min_x, min_y);
-    glVertex2f(max_x, min_y);
-    glVertex2f(min_x, min_y);
-    glVertex2f(min_x, max_y);
+    glVertex2f(0.98 * min_x + 0.02 * max_x, 0.98 * min_y + 0.02 * max_y);
+    glVertex2f(0.98 * max_x + 0.02 * min_x, 0.98 * min_y + 0.02 *max_y);
+    glVertex2f(0.98 * min_x + 0.02 * max_x, 0.98 * min_y + 0.02 *max_y);
+    glVertex2f(0.98 * min_x + 0.02 * max_x, 0.98 * max_y + 0.02 *min_y);
     glEnd();
 }
 void drawTicks() {
-    glColor3f(1.0, 1.0, 1.0);
-    for (float xx = min_x; xx <= max_x; xx +=(x_max-x_min)/20.0) {
+    glColor3f(1.0, 0, 0);
+    for (float xx = min_x; xx <= max_x; xx +=(max_x-min_x)/20.0) {
         glBegin(GL_LINES);
-        glVertex2f(xx, y_min);
-        glVertex2f(xx, y_min+0.05*(y_max-y_min));
+        glVertex2f(xx, min_y);
+        glVertex2f(xx, min_y+0.05*(max_y-min_y));
         glEnd();
-        glRasterPos2f(xx, y_min+0.06*(y_max-y_min));
+        glRasterPos2f(xx, min_y+0.06*(max_y-min_y));
         char buf[10];
-        sprintf_s(buf, sizeof(buf), "%.1f", xx);
+        snprintf(buf, sizeof(buf), "%.1f", xx);
         for (char* p = buf; *p; p++) {
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *p);
         }
     }
-    for (float yy = min_y; yy <= max_x; yy += (y_max - y_min) / 20.0) {
+    for (float yy = min_y; yy <= max_x; yy += (max_y - min_y) / 20.0) {
         glBegin(GL_LINES);
-        glVertex2f(x_min,yy);
-        glVertex2f(x_min + 0.05 * (y_max - y_min),yy);
+        glVertex2f(min_x,yy);
+        glVertex2f(min_x + 0.05 * (max_y - min_y),yy);
         glEnd();
-        glRasterPos2f(x_min + 0.06 * (x_max - x_min),yy);
+        glRasterPos2f(min_x + 0.06 * (max_x - min_x),yy);
         char buf[10];
-        sprintf_s(buf, sizeof(buf), "%.1f", yy);
+        snprintf(buf, sizeof(buf), "%.1f", yy);
         for (char* p = buf; *p; p++) {
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *p);
         }
