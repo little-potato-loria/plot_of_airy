4725f501c88143d3737266cea55acdc3dba112e3
little-potato-loria <tudoutudouyaaaaa@163.com>
2025-03-14 18:11:44 +0800

    Update globle.h

--- globle.h
+++ globle.h
@@ -1,18 +1,20 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 #define ChuanXing
-extern int NUM;
-extern float x0,v0,y0;
+#define NUM 4096
+#define _CRT_SECURE_NO_WARNINGS 1
+extern float x0,v0,y00;
 extern double dt;
-extern double yrem[NUM];
+extern double yrem[4096];
 extern float min_x, max_x, min_y, max_y;
 inline double func(double t, double yy, double v);
 void drawAxes();
 void drawTicks();
 #ifdef ChuanXing
-void DiTui(double* yy, double* v);
-void caculate(double yy, double v, double xx);
+void DiTui(double t, double* yy, double* v);
+void calculate(double yy, double v, double xx);
 #endif
 void display();
 void mouseClick(int button, int state, int x, int y);
+void keyboard(unsigned char key, int x, int y);
 #endif
